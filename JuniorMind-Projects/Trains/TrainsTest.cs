38aeb2043ae78d0f46eb2b7bdadfc1597ce5284b
Dan <dan.guita@yahoo.com>
2015-12-05 19:59:10 +0200

    Calculate Distance with given values

--- JuniorMind-Projects/Trains/TrainsTest.cs
+++ JuniorMind-Projects/Trains/TrainsTest.cs
@@ -4,11 +4,17 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Trains
 {
     [TestClass]
-    public class UnitTest1
+    public class TrainsTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Test1()
         {
+            float distantaZburata = CalculateDistance(200, 10, 20);
+            Assert.AreEqual(100, distantaZburata);
+        }
+        float CalculateDistance(float distantaTotala, float VitezaTren, float VitezaPasare)
+        {
+            return (distantaTotala / 2) / (VitezaTren * 2) * VitezaPasare;
         }
     }
 }
