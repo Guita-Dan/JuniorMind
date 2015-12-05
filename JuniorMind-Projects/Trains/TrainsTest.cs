3b9e3d5de7bee1ccd05bfa16e25d30521077094d
Dan <dan.guita@yahoo.com>
2015-12-05 20:03:27 +0200

    Create time variable and test2

--- JuniorMind-Projects/Trains/TrainsTest.cs
+++ JuniorMind-Projects/Trains/TrainsTest.cs
@@ -12,9 +12,16 @@ namespace Trains
             float distantaZburata = CalculateDistance(200, 10, 20);
             Assert.AreEqual(100, distantaZburata);
         }
+        [TestMethod]
+        public void Test2()
+        {
+            float distantaZburata = CalculateDistance(300, 20, 40);
+            Assert.AreEqual(150, distantaZburata);
+        }
         float CalculateDistance(float distantaTotala, float VitezaTren, float VitezaPasare)
         {
-            return (distantaTotala / 2) / (VitezaTren * 2) * VitezaPasare;
+            float timp = (distantaTotala / 2) / (VitezaTren * 2);
+            return timp * VitezaPasare;
         }
     }
 }
