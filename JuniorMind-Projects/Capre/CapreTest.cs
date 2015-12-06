0213fc11a404342409d1ed5485761b061119fa70
Dan <dan.guita@yahoo.com>
2015-12-06 15:01:24 +0200

    Empty Goat Project

---
+++ JuniorMind-Projects/Capre/CapreTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Capre
+{
+    [TestClass]
+    public class CapreTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
