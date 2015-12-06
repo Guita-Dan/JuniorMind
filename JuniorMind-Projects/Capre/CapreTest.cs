438ee89942c733a159fac367f4911190aa1e61cc
Dan <dan.guita@yahoo.com>
2015-12-06 15:26:00 +0200

    Reverse Test

--- JuniorMind-Projects/Capre/CapreTest.cs
+++ JuniorMind-Projects/Capre/CapreTest.cs
@@ -12,6 +12,12 @@ namespace Capre
             float foodPerGoatPerDay = CalculateFoodPerDayPerGoat(1, 2, 4);
             Assert.AreEqual(2, foodPerGoatPerDay);
         }
+        [TestMethod]
+        public void FoodForSameGoats()
+        {
+            float foodPerGoatPerDay = CalculateFoodPerDayPerGoat(3, 4, 8);
+            Assert.AreEqual(8, foodPerGoatPerDay*3*4);
+        }
         float CalculateFoodPerDayPerGoat(float days, float goats,float food)
         {
             return food / days / goats;
