47e72492378a462760f292b4e5b507706a2933e5
Dan <dan.guita@yahoo.com>
2015-12-06 16:43:18 +0200

    Calculate Red ones with precise division values

---
+++ JuniorMind-Projects/Mushrooms/MushroomsTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Mushrooms
+{
+    [TestClass]
+    public class Shrooms
+    {
+        [TestMethod]
+        public void PreciseDivisionTest()
+        {
+            int redMushrooms = CalculateRedMushrooms(12, 3);
+            Assert.AreEqual(9, redMushrooms);
+        }
+        int CalculateRedMushrooms(int totalMushrooms, int NrOfTimesGreater)
+        {
+            return totalMushrooms - totalMushrooms / (NrOfTimesGreater + 1);
+        }
+    }
+}
