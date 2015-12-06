6387c0eeb7b0fc618fc81ef7307e2899b07f0288
Dan <dan.guita@yahoo.com>
2015-12-06 15:07:24 +0200

    Calculate food/goat/day and test

--- JuniorMind-Projects/Capre/CapreTest.cs
+++ JuniorMind-Projects/Capre/CapreTest.cs
@@ -7,9 +7,14 @@ namespace Capre
     public class CapreTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void FoodPerDayPerGoat()
         {
-
+            float foodPerGoatPerDay = CalculateFoodPerDayPerGoat(1, 2, 4);
+            Assert.AreEqual(2, foodPerGoatPerDay);
+        }
+        float CalculateFoodPerDayPerGoat(float days, float goats,float food)
+        {
+            return food / days / goats;
         }
     }
 }
