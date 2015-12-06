125054d93c261bfcea101fc0e2bf3efca32fd79e
Dan <dan.guita@yahoo.com>
2015-12-06 16:22:35 +0200

    Empty Mushrooms Project

---
+++ JuniorMind-Projects/Mushrooms/Shrooms.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Mushrooms
+{
+    [TestClass]
+    public class Shrooms
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
