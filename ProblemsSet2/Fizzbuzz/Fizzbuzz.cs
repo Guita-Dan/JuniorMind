8a4067714e7b413a9b27d14df3dd328ba2a818d7
Dan <dan.guita@yahoo.com>
2015-12-09 23:38:57 +0200

    More simplified condition with ternary operator

--- ProblemsSet2/Fizzbuzz/Fizzbuzz.cs
+++ ProblemsSet2/Fizzbuzz/Fizzbuzz.cs
@@ -28,13 +28,7 @@ namespace Fizzbuzz
         }
         string FizzBuzz(int nr)
         {
-           if (nr % 3 == 0 && nr % 5 == 0)
-                return "FizzBuzz";
-            if (nr % 3 == 0)
-                return "Fizz";
-            if (nr % 5 == 0)
-                return "Buzz";
-            return nr.ToString();        
+            return (nr % 3==0) ? ((nr % 5==0) ? "FizzBuzz" : "Fizz") : ((nr % 5==0) ? "Buzz" : nr.ToString());
         }
         
     }
