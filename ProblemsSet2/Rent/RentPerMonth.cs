860b555a7f590e498e87b4b160c318db268b05f6
Dan <dan.guita@yahoo.com>
2015-12-10 14:14:41 +0200

    Calculate Rent for period between 30 and 40

--- ProblemsSet2/Rent/RentPerMonth.cs
+++ ProblemsSet2/Rent/RentPerMonth.cs
@@ -14,12 +14,19 @@ namespace Rent
         [TestMethod]
         public void CalculateRentForElevenToThirtyDaysPenalty()
         {
-            Assert.AreEqual(155, CalculateRentPerMonth(100, 15));
+            Assert.AreEqual(175, CalculateRentPerMonth(100, 15));
+        }
+        [TestMethod]
+        public void CalculateRentForElevenThirtyToFourtyDaysPenalty()
+        {
+            Assert.AreEqual(500, CalculateRentPerMonth(100, 40));
         }
         decimal CalculateRentPerMonth(int RentPrice, int NumberOfDays)
         {
-            if(NumberOfDays>10&&NumberOfDays<30)
-                return RentPrice + (RentPrice * 5 * NumberOfDays)/100 -20;
+            if(NumberOfDays>10&&NumberOfDays<=30)
+                return RentPrice + (RentPrice * 5 * NumberOfDays) / 100;
+            if(NumberOfDays > 30 && NumberOfDays <= 40)
+                return RentPrice + (RentPrice * 10 * NumberOfDays) / 100;
             return RentPrice+RentPrice*2*NumberOfDays/100;
         }
     }
