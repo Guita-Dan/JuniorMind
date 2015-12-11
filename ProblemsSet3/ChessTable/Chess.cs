fc43ed99c7335d53a77c76fefc61ffd93f1287f1
Dan <dan.guita@yahoo.com>
2015-12-11 18:19:56 +0200

    Calculate for bigger number of rows

--- ProblemsSet3/ChessTable/Chess.cs
+++ ProblemsSet3/ChessTable/Chess.cs
@@ -11,9 +11,22 @@ namespace ChessTable
         {
             Assert.AreEqual(1, CalculateNumberOfSquares(1));
         }
+        [TestMethod]
+        public void SquaresForTwoRows()
+        {
+            Assert.AreEqual(5, CalculateNumberOfSquares(2));
+        }
+        [TestMethod]
+        public void SquaresForEightRows()
+        {
+            Assert.AreEqual(204, CalculateNumberOfSquares(8));
+        }
         int CalculateNumberOfSquares(int numberOfRowsAndColumns)
         {
+            if (numberOfRowsAndColumns > 1)
+                return numberOfRowsAndColumns * numberOfRowsAndColumns + CalculateNumberOfSquares(numberOfRowsAndColumns - 1);
             return numberOfRowsAndColumns;
+
         }
     }
 }
