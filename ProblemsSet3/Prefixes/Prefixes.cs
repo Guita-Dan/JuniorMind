982fedcacc6500827697482f0f77255d27d5f63c
Dan <dan.guita@yahoo.com>
2015-12-13 17:34:47 +0200

    Finding common substring

--- ProblemsSet3/Prefixes/Prefixes.cs
+++ ProblemsSet3/Prefixes/Prefixes.cs
@@ -7,9 +7,19 @@ namespace Prefixes
     public class Prefixes
     {
         [TestMethod]
-        public void TestMethod1()
+        public void CommonPrefix()
         {
+            Assert.AreEqual("aaa", FindCommonSubstring("aaab", "aaadbg"));
         }
-        
+        string FindCommonSubstring(string firstString, string secondString)
+        {
+            int index=0;
+            for(int i=0; i < firstString.Length; i++)
+                if(firstString[i] == secondString[i])
+                {
+                    index = firstString.LastIndexOf(firstString[i]) + 1;
+                }
+           return firstString.Substring(0, index);
+        }
     }
 }
