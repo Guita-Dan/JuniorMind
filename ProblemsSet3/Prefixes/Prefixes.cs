3ea187cef4ac1961291317417f1574f7b1b5c04c
Dan <dan.guita@yahoo.com>
2015-12-13 19:39:57 +0200

    Finding common string when first string is longer

--- ProblemsSet3/Prefixes/Prefixes.cs
+++ ProblemsSet3/Prefixes/Prefixes.cs
@@ -16,18 +16,29 @@ namespace Prefixes
         {
             Assert.AreEqual("no common prefixes", FindCommonSubstring("aaab", "baadbg"));
         }
+        [TestMethod]
+        public void FirstStringLonger()
+        {
+            Assert.AreEqual("abba", FindCommonSubstring("abbaaaaaa","abbar"));
+        }
         string FindCommonSubstring(string firstString, string secondString)
         {
+            int min = firstString.Length;
+            if (firstString.Length > secondString.Length)
+                min = secondString.Length;
             int index=0;
-            for (int i = 0; i < firstString.Length; i++)
-                if (firstString[i] != secondString[i])
-                    break;
-                else
-                    index = firstString.LastIndexOf(firstString[i]) + 1;
+            for (int i = 0; i < min; i++)
+                    if (firstString[i] != secondString[i])
+                        break;
+                    else
+                        if (firstString.Length > secondString.Length)
+                            index = secondString.LastIndexOf(secondString[i]);
+                        else
+                            index = firstString.LastIndexOf(firstString[i]);
             if (index == 0)
                 return "no common prefixes";
             else
-                return firstString.Substring(0, index);
+                return firstString.Substring(0, index + 1);
         }
     }
 }
