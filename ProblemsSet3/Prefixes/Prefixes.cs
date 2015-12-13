06c2bb8b5146346e800921460c8a1f9c2e7d62c7
Dan <dan.guita@yahoo.com>
2015-12-13 17:52:33 +0200

    No common Prefixes fix

--- ProblemsSet3/Prefixes/Prefixes.cs
+++ ProblemsSet3/Prefixes/Prefixes.cs
@@ -11,15 +11,23 @@ namespace Prefixes
         {
             Assert.AreEqual("aaa", FindCommonSubstring("aaab", "aaadbg"));
         }
+        [TestMethod]
+        public void DifferentPrefix()
+        {
+            Assert.AreEqual("no common prefixes", FindCommonSubstring("aaab", "baadbg"));
+        }
         string FindCommonSubstring(string firstString, string secondString)
         {
             int index=0;
-            for(int i=0; i < firstString.Length; i++)
-                if(firstString[i] == secondString[i])
-                {
+            for (int i = 0; i < firstString.Length; i++)
+                if (firstString[i] != secondString[i])
+                    break;
+                else
                     index = firstString.LastIndexOf(firstString[i]) + 1;
-                }
-           return firstString.Substring(0, index);
+            if (index == 0)
+                return "no common prefixes";
+            else
+                return firstString.Substring(0, index);
         }
     }
 }
