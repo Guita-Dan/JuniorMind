3625634ccea0311c256ea7c675aec7cf5e2e0b99
Dan <dan.guita@yahoo.com>
2016-01-16 18:36:57 +0200

    Empty Cubes project

---
+++ ProblemsSet3/Cubes/Cube.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Cubes
+{
+    [TestClass]
+    public class Cube
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
