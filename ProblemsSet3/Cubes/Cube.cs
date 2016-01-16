333841bf166dde447c0a27581253090536fe3162
Dan <dan.guita@yahoo.com>
2016-01-16 20:42:16 +0200

    Calculate Number To Be Determined

--- ProblemsSet3/Cubes/Cube.cs
+++ ProblemsSet3/Cubes/Cube.cs
@@ -11,13 +11,28 @@ namespace Cubes
         {
             Assert.AreEqual(true, VerifyTripleEightEnding(192));
         }
-        bool VerifyTripleEightEnding(long number)
+        [TestMethod]
+        public void NumberWithTripleEightEnding()
         {
-            if(Math.Pow(number, 3) % 1000 == 888)
+            Assert.AreEqual(442, CalculateNumber(2));
+        }
+
+        int CalculateNumber(int nthNumberToBeDetermined)
+        {
+            int numberToBeDetermined = 192 + (nthNumberToBeDetermined - 1) * 250;
+            if (VerifyTripleEightEnding(numberToBeDetermined) == true)
+                return numberToBeDetermined;
+            else
+                return 0;
+        }
+
+        bool VerifyTripleEightEnding(long numberToBeDetermined)
+        {
+            if (Math.Pow(numberToBeDetermined, 3) % 1000 == 888)
                 return true;
             else
                 return false;
-                    
+
         }
     }
 }
