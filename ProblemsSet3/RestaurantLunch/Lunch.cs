7aef286344f2200c15e4f485c97a7ede04748ed1
Dan <dan.guita@yahoo.com>
2016-01-20 10:28:50 +0200

    Add empty Lunch project

---
+++ ProblemsSet3/RestaurantLunch/Lunch.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RestaurantLunch
+{
+    [TestClass]
+    public class Lunch
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
