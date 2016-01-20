3bb8a4af9d657697cf16c762a6b454069754b3b3
Dan <dan.guita@yahoo.com>
2016-01-20 10:36:40 +0200

    Calculate Greatest Common Divider

--- ProblemsSet3/RestaurantLunch/Lunch.cs
+++ ProblemsSet3/RestaurantLunch/Lunch.cs
@@ -7,8 +7,25 @@ namespace RestaurantLunch
     public class Lunch
     {
         [TestMethod]
-        public void TestMethod1()
+        public void GreatestCommonDividerSecondNrGreater()
         {
+            Assert.AreEqual(2, CalculateGreatestCommonDivider(4, 6));
+        }
+        [TestMethod]
+        public void GreatestCommonDividerFirstNrGreater()
+        {
+            Assert.AreEqual(4, CalculateGreatestCommonDivider(8, 4));
+        }
+        int CalculateGreatestCommonDivider(int him, int me)
+        {
+            while (him != me)
+            {
+                if (him > me)
+                    him = him - me;
+                else
+                    me = me - him;
+            }
+            return him;
         }
     }
 }
