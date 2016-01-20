b0f2a59cbabfa3a8a366a756c2401df0ef192049
Dan <dan.guita@yahoo.com>
2016-01-20 10:25:02 +0200

    Calculate Anagrams With Repeated Characters

--- ProblemsSet3/Anagrams/Anagram.cs
+++ ProblemsSet3/Anagrams/Anagram.cs
@@ -21,12 +21,56 @@ namespace Anagrams
         {
             Assert.AreEqual(6, CalculateNrOfAnagrams("abc"));
         }
+        [TestMethod]
+        public void AnagramsWithOneRepeatedChar()
+        {
+            Assert.AreEqual(3, CalculateNrOfAnagrams("aab"));
+        }
+        [TestMethod]
+        public void AnagramsWithMoreRepeatedChar()
+        {
+            Assert.AreEqual(12600, CalculateNrOfAnagrams("aaaabbbccd"));
+        }
+
         int CalculateNrOfAnagrams(string word)
         {
-            int result = 1;
+            int result;
             if (word.Length == 0)
                 return 0;
-            for (int i = 1; i <= word.Length; i++)
+            result = CalculateFactorial(word.Length) / CountOfRepeatedChars(word);
+            return result;
+        }
+
+        int CountOfRepeatedChars(string inputstring)
+        {
+            int result = 0;
+            int p = 1;
+            for (char charToCheck = 'a'; charToCheck <= 'z'; charToCheck++)
+            {
+                result = CountOfChars(inputstring, charToCheck);
+                p *= CalculateFactorial(result);
+            }
+            return p;
+        }
+
+        private static int CountOfChars(string inputstring, char charToCheck)
+        {
+            int result = 0;
+            for (int i = 0; i < inputstring.Length; i++)
+            {
+                if (inputstring[i] == charToCheck)
+                {
+                    result++;
+                }
+            }
+            return result;
+        }
+
+
+        private static int CalculateFactorial(int length)
+        {
+            int result = 1;
+            for (int i = 1; i <= length; i++)
                 result *= i;
             return result;
         }
