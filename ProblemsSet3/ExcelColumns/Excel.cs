7862d00e1a6983608dc5a37d527f55fc20dbc73f
Dan <dan.guita@yahoo.com>
2016-01-24 10:27:45 +0200

    Return Single Letter Column Name

--- ProblemsSet3/ExcelColumns/Excel.cs
+++ ProblemsSet3/ExcelColumns/Excel.cs
@@ -7,8 +7,15 @@ namespace ExcelColumns
     public class Excel
     {
         [TestMethod]
-        public void TestMethod1()
+        public void SingleLetterTest()
         {
+            Assert.AreEqual("A", FindExcelColumnName(1));
+        }
+        string FindExcelColumnName(int columnNumber)
+        {
+            columnNumber = columnNumber - 1;
+            string columnName = String.Empty;
+            return columnName = Convert.ToChar(65 + columnNumber).ToString();
         }
     }
 }
