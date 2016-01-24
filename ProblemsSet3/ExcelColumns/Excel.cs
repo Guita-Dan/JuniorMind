a7d537329e20860ff13e54d88d3794d84c6e109a
Dan <dan.guita@yahoo.com>
2016-01-24 10:37:59 +0200

    Return Double Letter Column Names

--- ProblemsSet3/ExcelColumns/Excel.cs
+++ ProblemsSet3/ExcelColumns/Excel.cs
@@ -11,11 +11,22 @@ namespace ExcelColumns
         {
             Assert.AreEqual("A", FindExcelColumnName(1));
         }
+        [TestMethod]
+        public void DoubleLetterTest()
+        {
+            Assert.AreEqual("AB", FindExcelColumnName(28));
+        }
         string FindExcelColumnName(int columnNumber)
         {
-            columnNumber = columnNumber - 1;
+            int modulo;
             string columnName = String.Empty;
-            return columnName = Convert.ToChar(65 + columnNumber).ToString();
+            while (columnNumber > 0)
+            {
+                modulo = (columnNumber - 1) % 26;
+                columnName = Convert.ToChar(65 + modulo).ToString() + columnName;
+                columnNumber = (int)(columnNumber - modulo) / 26;
+            }
+            return columnName;
         }
     }
 }
